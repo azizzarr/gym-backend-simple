a062ea9c96842f1cf0179608ef8b8b636ceea36b
azizzarr <75136140+azizzarr@users.noreply.github.com>
2025-06-19 01:19:28 +0100

    PROD2-committTssqcb

--- src/main/java/com/gymapp/backend/BackendApplication.java
+++ src/main/java/com/gymapp/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package com.gymapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.gymapp.backend.repository")
+@EntityScan(basePackages = "com.gymapp.backend.model")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
